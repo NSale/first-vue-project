950f06b51c39d899b72f824aecfd5586e4b15368
NSale <agospodinovic84@gmail.com>
2022-02-15 23:49:26 +0100

    Add button, tasks and task

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks='tasks'/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      if (confirm('Toggle Reminder?')) {
+        this.tasks = this.tasks.map((task) => task.id === id 
+          ? {...task, reminder: !task.reminder} 
+          : task)}
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        "id": "1",
+        "text": "Doctors Appointment",
+        "day": "March 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": "2",
+        "text": "Meeting with boss",
+        "day": "March 6th at 1:30pm",
+        "reminder": true
+      },
+      {
+        "id": "3",
+        "text": "Food shopping",
+        "day": "March 7th at 2:00pm",
+        "reminder": false
+      }
+    ]
   }
 }
 </script>
